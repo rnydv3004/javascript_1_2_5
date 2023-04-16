34bf8aa2c473d8c545a6eb89ac03b6ba69b143af
rnydv3004 <yaryan3087@gmail.com>
2023-04-16 12:48:01 +0530

    Initial Commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const box = document.getElementById("box");
 
-box.addEventListener("dblclick",function (){
-    alert("You double click the box!");
+box.addEventListener("keypress",function (){
+    alert("You are pressing a key not the box");
 });
